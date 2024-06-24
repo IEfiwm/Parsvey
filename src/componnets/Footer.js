76aaea0db9147b805c6c9c028c198dc5fa5c2947
mahdimoosavi <seyed.mehdi137596@gmail.com>
2024-06-24 21:28:37 +0330

    add packages and edit home page

--- src/componnets/Footer.js
+++ src/componnets/Footer.js
@@ -1,53 +1,69 @@
-import React from 'react';
+import React from "react";
 import "../css/footer.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-   const handleme=()=>{
-    window.open("https://www.instagram.com/nima._.shafiee", "_blank");
-   }  
+const handleme = () => {
+  window.open("https://www.instagram.com/nima._.shafiee", "_blank");
+};
 
 function Footer() {
   return (
     <div>
-        <footer class="footer">
-    <div class="footer-left">
-        <h2>تجهیز صنعت پارس وی</h2>
-        <br/>
-        
-        <Link className="nav__link" to="/">خانه</Link>
-        <br/>
-        <br/>
-        <Link className="nav__link" to="/درباره ما">درباره ما</Link> 
-        <br/>
-        <br/>
-        <Link className="nav__link" to="/تماس باما"> تماس باما</Link> 
-        <br/>
-        <br/>
-        <Link className="nav__link"  onClick={handleme}>طراح سایت</Link> 
-        <br/>
-       
-    </div>
-    <div class="footer-center">
-        <h3> شماره های تماس </h3>
-        <br/>
-        <ul>
+      <footer class="footer">
+        <div class="footer-left">
+          <h2>تجهیز صنعت پارس وی</h2>
+          <br />
+
+          <Link className="nav__link" to="/">
+            خانه
+          </Link>
+          <br />
+          <br />
+          <Link className="nav__link" to="/درباره ما">
+            درباره ما
+          </Link>
+          <br />
+          <br />
+          <Link className="nav__link" to="/تماس باما">
+            {" "}
+            تماس باما
+          </Link>
+          <br />
+          <br />
+          <Link className="nav__link" onClick={handleme}>
+            طراح سایت
+          </Link>
+          <br />
+        </div>
+        <div class="footer-center">
+          <h3> شماره های تماس </h3>
+          <br />
+          <ul>
             <li>خانم مهندس شفیعی : 09913382521</li>
-            <br/>
+            <br />
             <li> اقای مهندس کریمی : 09057479475 </li>
-            <br/>
-        </ul>
+            <br />
+          </ul>
+        </div>
+        <div class="footer-right">
+          <h3>ایمیل :</h3>
+          <p>
+            {" "}
+            میتوانید ایمیل خود را ارسال کنید تا ما از طریق ایمیل با شما در
+            ارتباط باشیم
+          </p>
+          <br />
+          <input
+            type="email"
+            className="inptemail"
+            placeholder="لطفا ایمیل خود را وارد کنید ..."
+          />
+          <br />
+          <button className="mybtn3">تایید</button>
+        </div>
+      </footer>
     </div>
-    <div class="footer-right">
-        <h3>ایمیل :</h3>
-        <p> میتوانید ایمیل خود را ارسال کنید تا ما از طریق ایمیل با شما در ارتباط باشیم</p>
-        <br/>
-        <input type="email" className='inptemail' placeholder="لطفا ایمل خود را ئارد کید"/>
-        <br/>
-        <button className='mybtn3'>تایید</button>
-    </div>
-</footer>
-    </div>
-  )
+  );
 }
 
 export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,96 @@
-import React from 'react';
-import Header from '../componnets/Header';
+import React from "react";
+import Header from "../componnets/Header";
 //import firstphoto from "../photos/firstimg.JPG";
-import secondphoto from "../Photos/secondimg.JPG" ;
-import forthphoto from '../Photos/forth.JPG' ;
-import '../css/Home.css'
-import Footer from '../componnets/Footer';
-import ScrollToTop from '../componnets/ScrollToTop';
-
+import secondphoto from "../Photos/secondimg.JPG";
+import forthphoto from "../Photos/forth.JPG";
+import "../css/Home.css";
+import Footer from "../componnets/Footer";
+import ScrollToTop from "../componnets/ScrollToTop";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <React.Fragment>
-    <Header/>
-    <div className='myhome'>
-        
-        <h2 className='myh'>  با کمک ما از بهترین تجهیزات استقاده کنید :<br/> <p1 className="myp">برای خرید ماشینهای بستنی ساز و سردخانه های صنعتی<br/> و تجهیزات کافی شاپ با برند های متنوع<br/> از جمله تهویه مطبوع و چیلر و فن کویل <br/> و هواساز وتجهیزات نفت و گاز و موارد دیگر <br/> به صورت نقد واقساط که در ادامه سایت ذکر شده میتوانید<br/> با ما هم به صورت حضوری و هم به صورت تلفنی و یا <br/>پیام رسان های مختلف درارتباط باشید <br/><br/>
-      
-        </p1> <img className='secondphoto' src={secondphoto} alt='divphotos' /></h2>
-        <br/>
-        <br/>
+      <Header />
+      {/* <div className="flex flex-col">
+        <h2 className="myh"> با کمک ما از بهترین تجهیزات استقاده کنید : </h2>
+      </div> */}
+      <div className="myhome">
+        <div className="flex flex-col mt-8">
+          <h2 className="text-2xl pr-4 mb-8">
+            با کمک ما از بهترین تجهیزات استفاده کنید :
+          </h2>
+          <div className="md:flex-row flex gap-2 flex-col justify-between">
+            <div className="md:w-1/2 pr-2 text-xl">
+              برای خرید ماشینهای بستنی ساز و سردخانه های صنعتی و تجهیزات کافی
+              شاپ با برند های متنوع از جمله تهویه مطبوع و چیلر و فن کویل و
+              هواساز وتجهیزات نفت و گاز و موارد دیگر به صورت نقد واقساط که در
+              ادامه سایت ذکر شده میتوانید با ما هم به صورت حضوری و هم به صورت
+              تلفنی و یا پیام رسان های مختلف درارتباط باشید .
+            </div>
+            <div className="md:w-1/2 px-2 md:px-0">
+              <img
+                className="w-full rounded-lg md:rounded-none md:rounded-tr-[20px] md:rounded-br-[20px]"
+                src={secondphoto}
+                alt="divphotos"
+              />
+            </div>
+          </div>
+        </div>
 
-        <br/>
+        <div className="btndiv !my-12">
+          <button className="mybtn !mt-0">
+            <Link className="nav__link custom" to="/تماس باما">
+              تماس با ما
+            </Link>
+          </button>
+          <button className="mybtn2">
+            <Link className="nav__link custom" to="/درباره ما">
+              درباره ما
+            </Link>
+          </button>
+        </div>
 
-        <br/>
-
-      <div className='btndiv'> <button className='mybtn'> تماس با ما</button> <button className='mybtn2'>درباره ما</button> 
-      </div>  
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-
-      <div className='dardesar'>
-       <h2 className='myh2'>آیا محصولات را میتوان به صورت اقساط خریداری کرد ؟<br/><p1 className="myp2"> در جواب به این سوال باید گفت که بله شما میتوانید تمامی محصولات مارا از جمله چیلر<br/> و فن کویل و هواساز و تهویه مطبوع و  محصولات دیگر را هم به صورت نقدی <br/>و هم به صورت اقساط خریداری کنید <br/> برای دریافت اطلاعات بیشتر درمورد چگونگی<br/>اقساط و مدت زمان اقساط میتوانید<br/> با  در ارتباط باشید  ...</p1><img className='forthphoto' src={forthphoto} alt='photos'/></h2>
-        
+        <div className="flex flex-col my-8">
+          <h2 className="text-2xl pr-4 mb-8">
+            آیا محصولات را میتوان به صورت اقساط خریداری کرد ؟
+          </h2>
+          <div className="md:flex-row flex gap-2 flex-col justify-between">
+            <div className="md:w-1/2 px-2 md:px-0">
+              <img
+                className="w-full rounded-lg md:rounded-none md:rounded-tl-[20px] md:rounded-bl-[20px]"
+                src={forthphoto}
+                alt="photos"
+              />
+            </div>
+            <div className="md:w-1/2 px-2 text-xl">
+              در جواب به این سوال باید گفت که بله شما میتوانید تمامی محصولات
+              مارا از جمله چیلر و فن کویل و هواساز و تهویه مطبوع و محصولات دیگر
+              را هم به صورت نقدی و هم به صورت اقساط خریداری کنید برای دریافت
+              اطلاعات بیشتر درمورد چگونگی اقساط و مدت زمان اقساط میتوانید با در
+              ارتباط باشید ...
+            </div>
+          </div>
+        </div>
+        {/* <div className="dardesar">
+          <h2 className="myh2">
+            آیا محصولات را میتوان به صورت اقساط خریداری کرد ؟
+            <p1 className="myp2">
+              {" "}
+              در جواب به این سوال باید گفت که بله شما میتوانید تمامی محصولات
+              مارا از جمله چیلر و فن کویل و هواساز و تهویه مطبوع و محصولات دیگر
+              را هم به صورت نقدی و هم به صورت اقساط خریداری کنید برای دریافت
+              اطلاعات بیشتر درمورد چگونگی اقساط و مدت زمان اقساط میتوانید با در
+              ارتباط باشید ...
+            </p1>
+            <img className="forthphoto" src={forthphoto} alt="photos" />
+          </h2>
+        </div> */}
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-
-      <br/>
-      <br/>
-      <br/>
-
-    </div>
-    <ScrollToTop/>
-    <Footer/>
+      <ScrollToTop />
+      <Footer />
     </React.Fragment>
-  )
+  );
 }
 
-export default Home
+export default Home;
